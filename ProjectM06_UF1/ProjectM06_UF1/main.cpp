0ccb1db878cd96403a7972afa7fc658d59100503
Iker <perikinperikon@gmail.com>
2019-03-28 14:32:50 +0100

    Bueno...

--- ProjectM06_UF1/ProjectM06_UF1/main.cpp
+++ ProjectM06_UF1/ProjectM06_UF1/main.cpp
@@ -2,9 +2,65 @@
 #include <fstream>
 #include <string>
 #include <array>
+#include "raylib.h"
+
+struct Level {
+	std::string title;
+	int number;
+	int width;
+	int height;
+	int** background;
+	std::string* collisions;
+	int** foreground;
+};
+Level* levelsStruct;
+
+int loadBackground(std::ifstream &file, Level &s1);
+
+/*int loadLevels(std::ifstream& file, int num) {
+	int w = 12;
+	int h = 8;
+	int nume_level;
+	std::string(title);
+
+	std::string word;
+
+	std::getline(file, word, ';');
+	num_level = std::stoi(file, word, ';');
+
+	std::getline(file, word, ';');
+	title = word;
+
+	if(word.compare("LEVEL") != 0)
+		return 1;
+	std::getline(file, word, ';');
+
+	levels[i].background = new int[w];
+	levels[i].collisions = new std::string[w];
+	levels[i].foreground = new int[w];
+
+	for (int j = 0; j < q; j++) {
+		levels[i].background[j] = new int[h];
+		levels[i].collisions[j] = new std::string[h];
+		levels[i].foreground[j] = new int[h];
+	}
+
+	for (int k = 0; k < 3; k++) {
+		std::getline(file, word, ';');
+		std::string block = word;
+		std::getline(file, word, '\n');
+
+		if (word.compare("BACKGROUND") == 0)
+			loadBackground(file, w, h);
+		else if (word.compare("FOREGROUND") == 0)
+			loadForeground(file, w, h);
+		else if (word.compare("COLLISIOBS") == 0)
+			loadCollisions(file, w, h);
+	}
+}*/
 
 int main() {
-	std::ofstream archivo;
+	/*std::ofstream archivo;
 	
 	archivo.open("datos.csv");
 
@@ -15,39 +71,6 @@ int main() {
 	archivo.close();
 
 	return 0;
-
-
-
-	/*std::ifstream ip("../ProjectM06_UF1/personas.csv");
-
-	if (!ip.is_open()) {
-		std::cout << "ERROR: Error al abrir el archivo" << "\n";
-		return 1;
-	}
-
-	std::string nombre;
-	std::string apellido;
-	std::string	edad;
-	std::string peso;
-
-	while (ip.good()) {
-		std::getline(ip, nombre, ';');
-		std::getline(ip, apellido, ';');
-
-		std::getline(ip, edad, ';');
-		int edadInt = stoi(edad);
-
-		std::getline(ip, peso, '\n');
-		float pesoInt = stof(peso);
-
-		std::cout << "Nombre: " << nombre << " " << apellido << "\n";
-		std::cout << "Edad: " << edadInt << "\n";
-		std::cout << "Peso: " << pesoInt << "\n";
-		std::cout << "=========================" << "\n";
-	}
-}
-	
-	ip.close();*/
 
 	std::ifstream archivo2("../ProjectM06/game.csv");
 	std::string parameter;
@@ -76,5 +99,42 @@ int main() {
 				std::getline(archivo2, titulo, ';');
 			};
 		};
-	};
+	};*/
+
+	// Initialization
+	//--------------------------------------------------------------------------------------
+	int screenWidth = 800;
+	int screenHeight = 450;
+
+	InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+	SetTargetFPS(60);
+	//--------------------------------------------------------------------------------------
+
+	// Main game loop
+	while (!WindowShouldClose())    // Detect window close button or ESC key
+	{
+		// Update
+		//----------------------------------------------------------------------------------
+		// TODO: Update your variables here
+		//----------------------------------------------------------------------------------
+
+		// Draw
+		//----------------------------------------------------------------------------------
+		BeginDrawing();
+
+		ClearBackground(BLUE);
+
+		DrawText("He echo una ventana y no me gusta para nada", 190, 200, 20, RED);
+
+		EndDrawing();
+		//----------------------------------------------------------------------------------
+	}
+
+	// De-Initialization
+	//--------------------------------------------------------------------------------------   
+	CloseWindow();        // Close window and OpenGL context
+	//--------------------------------------------------------------------------------------
+
+	return 0;
 }
